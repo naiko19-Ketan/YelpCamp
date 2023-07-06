a5549700fa40e62bc64c67f230cc3f2e9a8a60e5
Ketan <ketanm161@gmail.com>
2023-07-06 17:44:08 +0530

    Added flash prompt

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 const campgrounds = require("./routes/campgrounds.js");
 const reviews = require("./routes/reviews.js");
@@ -25,6 +26,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
@@ -38,9 +40,17 @@ const sessionConfig = {
 	},
 };
 
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+	res.locals.success = req.flash("success");
+	res.locals.error = req.flash("error");
+	next();
+});
+
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
-app.use(session(sessionConfig));
 
 app.get("/", (req, res) => {
 	res.render("home");
