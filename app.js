86dfda24d18af265a8fbdbe9c74fe05d5e27cecf
Ketan <ketanm161@gmail.com>
2023-07-08 22:47:24 +0530

    included initial passport authentication

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgrounds = require("./routes/campgrounds.js");
 const reviews = require("./routes/reviews.js");
@@ -43,10 +46,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
 	res.locals.success = req.flash("success");
 	res.locals.error = req.flash("error");
 	next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+	const user = new User({ email: "naughty@gmail.com", username: "naughty" });
+	const newUser = await User.register(user, "chicken");
+	res.send(newUser);
 });
 
 app.use("/campgrounds", campgrounds);
